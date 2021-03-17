979ff1857897f55e72830bfe5dac3d233f3b0d6d
Aliaksei Hladki1 <aliaksei_hladki1@epam.com>
2021-03-17 19:16:33 +0300

    Homework6: Aliaksei_Hladki1 chenged tests

--- flask/handlers/pulls.py
+++ flask/handlers/pulls.py
@@ -4,22 +4,34 @@ token = {'Authorization': 'token'}
 
 
 def get_pulls(state):
+    return get_pulls_get(state)[1]
+
+
+def get_pulls_get(state):
     resp = 'https://api.github.com/repos/alenaPy/devops_lab/pulls'
     resp_search = 'https://api.github.com/search/issues?q=is:pr%20label:'
+    item = requests.get(resp, headers=token)
 
     if state in ['open', 'closed']:
-        resp = requests.get(resp,
+        item = requests.get(resp,
                             headers=token,
                             params={'state': '{0}'.format(state),
                                     'per_page': '100'})
-        return resp.json()
-
-    if state in ['accepted', 'needs work']:
-        resp = requests.get(resp_search + f'\"{state}\"\
+        return item.status_code, get_inform(state, item.json())
+    elif state in ['accepted', 'needs work']:
+        item = requests.get(resp_search + f'\"{state}\"\
                    %20repo:alenaPy/devops_lab&per_page=100',
                             headers=token,
                             params={'per_page': '100'})
-        return resp.json()["items"]
+        return item.status_code, get_inform(state, item.json())
+    else:
 
-    return requests.get(resp,
-                        headers=token).json()
+        return item.status_code, get_inform(state, item.json())
+
+
+def get_inform(state, it):
+    if state in ['open', 'closed']:
+        return it
+    if state in ['accepted', 'needs work']:
+        return it['items']
+    return it
--- flask/test/tests.py
+++ flask/test/tests.py
@@ -7,16 +7,18 @@ class TestPrime(TestCase):
     def setUp(self):
         """Init"""
 
-    def test_exists_true(self):
-        self.assertTrue(pulls.get_pulls("open")[0]["state"], "open")
-        self.assertTrue(pulls.get_pulls("closed")[0]["state"], "closed")
+    def test_exists_open(self):
+        self.assertTrue(pulls.get_inform("open", [{"state": "open"}])[0]["state"] == "open")
+        self.assertTrue(pulls.get_inform("all", [{"state": "open"}])[0]["state"] == "open")
+        self.assertTrue(pulls.get_inform("accepted", {"items": "work"}) == "work")
 
     def test_len(self):
-        self.assertNotEqual(len(pulls.get_pulls("open")), 0)
-        self.assertNotEqual(len(pulls.get_pulls("closed")), 0)
-        self.assertNotEqual(len(pulls.get_pulls("accepted")), 0)
-        self.assertNotEqual(len(pulls.get_pulls("needs work")), 0)
-        self.assertNotEqual(len(pulls.get_pulls("all")), 0)
+        self.assertNotEqual(len(pulls.get_inform("open", [{"state": "open"}])), 0)
+        self.assertNotEqual(len(pulls.get_inform("accepted", {"items": "work"})), 0)
+        self.assertNotEqual(len(pulls.get_inform("all", [{"state": "all"}])), 0)
+
+    def test_code(self):
+        self.assertTrue(pulls.get_pulls_get("open")[0] == 200)
 
     def tearDown(self):
         """Finish"""
